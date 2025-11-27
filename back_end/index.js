07f6cbb7393fe3eda4387fd054948c4aabe7f009
AyseeD <ayse.delen@yandex.com>
2025-11-27 15:22:14 +0300

    add necessary dependencies for back end and connect to database

--- back_end/index.js
+++ back_end/index.js
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.get("/", (req,res)=>{
+    console.log("hello");
+})
+
 app.listen(port, ()=>{
     console.log(`Lisening on port ${port}`)
 })
